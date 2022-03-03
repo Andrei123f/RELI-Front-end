6cddb2bf12f8eaa0c3dac915e3c80792a28c1e15
andrei <andrei.popa@technologi.co.uk>
2022-03-03 11:58:12 +0000

    initial commit 2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from "vue";
+// styles
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+// mouting point for the whole app
+import App from "@/App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,85 @@
 import { createWebHistory, createRouter } from "vue-router";
-import test from "../views/Test.vue";
-import DefaultV from '../views/Default.vue';
-import Andrei from '../views/Andrei.vue';
+import Admin from "@/layouts/Admin.vue";
+import Auth from "@/layouts/Auth.vue";
+
+// layouts
+// views for Admin layout
+
+import Dashboard from "@/views/admin/Dashboard.vue";
+import Settings from "@/views/admin/Settings.vue";
+import Tables from "@/views/admin/Tables.vue";
+import Maps from "@/views/admin/Maps.vue";
+
+// views for Auth layout
+
+import Login from "@/views/auth/Login.vue";
+import Register from "@/views/auth/Register.vue";
+
+// views without layouts
+
+import Landing from "@/views/Landing.vue";
+import Profile from "@/views/Profile.vue";
+import Index from "@/views/Index.vue";
+
+// routes
 
 const routes = [
-    {
-        path: '/',
-        name: 'Default Vue Page',
-        component: DefaultV
-    },
-
-    {
-        path: '/test',
-        name: 'Test',
-        component: test
-    },
-    
-    {
-        path: '/andrei',
-        name: 'Andrei',
-        component: Andrei
-    }  
+  {
+    path: "/admin",
+    redirect: "/admin/dashboard",
+    component: Admin,
+    children: [
+      {
+        path: "/admin/dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/admin/settings",
+        component: Settings,
+      },
+      {
+        path: "/admin/tables",
+        component: Tables,
+      },
+      {
+        path: "/admin/maps",
+        component: Maps,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/auth/login",
+    component: Auth,
+    children: [
+      {
+        path: "/auth/login",
+        component: Login,
+      },
+      {
+        path: "/auth/register",
+        component: Register,
+      },
+    ],
+  },
+  {
+    path: "/landing",
+    component: Landing,
+  },
+  {
+    path: "/profile",
+    component: Profile,
+  },
+  {
+    path: "/",
+    component: Index,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/" },
 ];
 
-
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
