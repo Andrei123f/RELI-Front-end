5f1270bf3fe564848e603a20e26d24e27a756dc0
andrei <andrei.popa@technologi.co.uk>
2022-03-04 14:46:02 +0000

    vite heroku fix 7

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,4 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: process.env.PORT || 3000
-  },
-  preview: {
-    port: process.env.PORT || 3000
-  }
-})
+  plugins: [vue()]})
