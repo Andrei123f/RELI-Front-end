3507f511380bdeda44a34e52f16792a8d100d8ff
andrei <andrei.popa@technologi.co.uk>
2022-03-11 18:23:58 +0000

    writes pseudocode for getting the dashboard data

--- src/store/dashboardStore.js
+++ src/store/dashboardStore.js
@@ -11,18 +11,29 @@ const dashboardStore = {
   },
   mutations: {},
   actions: {
+  
     async getDashboardData({ dispatch, rootGetters }) {
+      //TODO call the authStore to see if we have a token
+      //if we have a token, call a new API endpoint to see if that token is still usable
+      //if that token is still usable, retrieve it
+      //if that token is not still usable, get a new one and update it.
+      
+      /*
       const accessToken = rootGetters['authStore/getAccessToken'];
-      console.log(accessToken);
+
       const response = await axios.get(API_URL + "challenge/getStats", {
           headers: {
               'Authorization': `Bearer ${accessToken}`
           }
-      });
+      }).catch((err) => {
+        if(err.response.status == 403){
+          console.log('needs to refresh the access token');
+        }
+      })
+      return response;
       //now based on the response decide if you need to refresh the access token or not.
       //and whenever we come back to the page not to make the same request again...
-      console.log(response);
-      return response;
+      */
     },
   },
   getters: {},
