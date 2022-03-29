f87356ea1eb334ef4284b627e27b62b7a586b0a4
andrei <andrei.popa@technologi.co.uk>
2022-03-29 18:36:51 +0100

    forcing vercel to deploy

--- src/components/dashboard/challenge/Terminal.vue
+++ src/components/dashboard/challenge/Terminal.vue
@@ -17,7 +17,6 @@
 import { PrismEditor } from "vue-prism-editor";
 import "vue-prism-editor/dist/prismeditor.min.css";
 
-// import highlighting library (you can use any library you want just return html string)
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
