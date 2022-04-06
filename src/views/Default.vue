ffb94b3d7c35c63f23d9d05ac7dd509e732402ee
andrei <andrei.popa@technologi.co.uk>
2022-04-06 14:38:32 +0100

    finishes the project structure

---
+++ src/views/Default.vue
@@ -0,0 +1,4 @@
+<template>
+    Well, this is awkward, you shouldn't have ended up here... but you did for some reason. <br />
+    <router-link to="/"> <div class="btn">Go back to safety</div></router-link>
+</template>
